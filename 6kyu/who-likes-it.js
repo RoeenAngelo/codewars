b64a449a8806a545d7071e585fa2f9802ea73dfc
Roeen <roeenacal@gmail.com>
2023-04-13 08:05:16 -0700

    Add new codewars challenge and solution

--- 6kyu/who-likes-it.js
+++ 6kyu/who-likes-it.js
@@ -1,3 +1,20 @@
+DESCRIPTION:
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+
+
+// Solution
+
 // function likes(names) {
 //     for (i=0 ; i<names.length; i++) {
 //         if (names == null || names.Length == 0) {
@@ -18,6 +35,8 @@
 //        }
 // }
 
+// Solution
+
 function likes(names) {
     const [first, second, third] = names;
     switch (names.length) {
@@ -30,4 +49,3 @@ function likes(names) {
   }
 
 console.log(likes([]));
-// console.log(likes(['Alex']));
