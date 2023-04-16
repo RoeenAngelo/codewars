c9f81b0feaaf769ba1532f787fc1867b2d6080e4
Roeen <roeenacal@gmail.com>
2023-04-16 08:11:34 -0700

    Add new codewars challenge and solution

--- 8kyu/calculate-bmi.js
+++ 8kyu/calculate-bmi.js
@@ -12,22 +12,22 @@
 
 function bmi(weight, height) {
   
-    let result  = weight / height**2
+    let bmi  = weight / height**2
 
         return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
 
 }
 
 //   function bmi(weight, height) {
-//     let result = weight / height**2
+//     let bmi = weight / height**2
    
-//         if (result <= 18.5){
+//         if (bmi <= 18.5){
 //             return 'Underweight';
 //         }
-//         else if (result <= 25) {
+//         else if (bmi <= 25) {
 //             return 'Normal';
 //         }
-//         else if (result <=30){
+//         else if (bmi <=30){
 //             return 'Overweight'
 //         }
 //         else 
