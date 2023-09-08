241a06534b38ab271e9e8b32a0d96ae69d8e6153
Roeen <roeenacal@gmail.com>
2023-09-08 11:36:08 -0700

    Add problem and solution

--- 8kyu/is-it-even.js
+++ 8kyu/is-it-even.js
@@ -8,47 +8,9 @@
 
 // Floats with decimal part non equal to zero are considered UNeven for this kata.
 
-// function testEven(n) {
-//   return n % 2 === 0 ? true : false
-// }
-
-// console.log(testEven(9))
-
-/*
-  1. Convert string to an array
-  2. Create a new empty array named final
-  3. Iterate over the array using for loop
-  4. If A supercedes a number, add one to that number using the addOne function and push it to the newArr
-  5. If B supercedes a number, subtract one to that number using the subtraction function ""
-  6. else push the element to the new array
-
-*/
-
-const addOne = num => num + 1
-const subOne = num => num - 1
-
-
-function usePotions(str) {
-  const finalArr = []
-  const strArr = str.split('')
-
-  for( let i = 0; i < strArr.length; i++) {
-    if(strArr[i] === 'A') {
-      finalArr.push(addOne(+strArr[i-1]) )
-    }
-    else if (strArr[i] === 'B') {
-      finalArr.push(subOne(+strArr[i-1] ))
-    }
-    else if (strArr[i+1] !== 'A' && strArr[i+1] !== 'B') {
-      finalArr.push(+strArr[i])
-    }
-    // else {
-    //   finalArr.push(+strArr[i])
-    // }
-  }
-
-  return finalArr.join('').toString()
-
+function testEven(n) {
+  return n % 2 === 0 ? true : false
 }
 
-console.log(usePotions('3A78B51'))
+console.log(testEven(9))
+
