cdf43a01fb9b0e91584c73f7d028a803103f52c4
Roeen <roeenacal@gmail.com>
2023-10-05 12:57:06 -0600

    Add new problem and solution

---
+++ freeCodeCamp/missing-letters.js
@@ -0,0 +1,26 @@
+// Missing letters
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+// fearNotLetter("abce") should return the string d.
+// fearNotLetter("abcdefghjklmno") should return the string i.
+// fearNotLetter("stvwx") should return the string u.
+// fearNotLetter("bcdf") should return the string e.
+// fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
+
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length; i++) {
+    /* code of current character */
+    const charCode = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+        then a letter was skipped */
+    if (charCode !== str.charCodeAt(0) + i) {
+      /* if current character skipped past a character find previous character and return */
+      return String.fromCharCode(charCode - 1);
+    }
+  }
+  return undefined;
+}
+fearNotLetter("abce");
